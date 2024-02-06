6b78b6121b5bc2e22a5847343411ee8c75adeb07
Lucas Gonçalves <lgg9083@gmail.com>
2024-02-06 09:02:11 -0300

    calculato vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Calculadora</h1>
+<CalculatorComponet/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalculatorComponet from './main/CalculatorComponet.vue'
+
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ components: { CalculatorComponet }
+  
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items:center ;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  color: #fff;
+  background: linear-gradient(to right rgb(83, 105, 118), rgb(41 ,46, 73));
 }
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: [
+    // Add the dependencies you want to transpile
+    // For example, if CalculatorComponent is a dependency, add it like this:
+    'CalculatorComponent',
+    // Add other dependencies if needed
+  ],
+});
